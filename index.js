0362f10d1fb5f56bed85cb53940e3410a1490c10
britchey <50119704+britchey1227@users.noreply.github.com>
2024-07-03 17:34:28 -0700

    Better logic for selecting pieces and sending moves
    only can send a piece of your color as a potentially valid move.
    also updated turn back to white on reset
    BUG: if white plays a move then black connects, black does not see the move then the board resets as if black has first move

--- index.js
+++ index.js
@@ -1,174 +1,193 @@
-window.addEventListener('DOMContentLoaded', () => {
-    let board = [
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '']
-    ];
+window.addEventListener("DOMContentLoaded", () => {
+  let board = [
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+  ];
 
-    const tiles = Array.from(document.querySelectorAll('.tile'));
-    colorBoard()
+  const tiles = Array.from(document.querySelectorAll(".tile"));
+  colorBoard();
 
-    const audio = document.getElementById("audio");
-    // const playerDisplay = document.querySelector('.display-player');
-    const resetButton = document.querySelector('#reset');
-    const announcer = document.querySelector('.announcer');
+  const audio = document.getElementById("audio");
+  // const playerDisplay = document.querySelector('.display-player');
+  const resetButton = document.querySelector("#reset");
+  const announcer = document.querySelector(".announcer");
 
-    let currentPlayer = null;
-    let isGameActive = true;
+  let currentPlayer = null;
+  let playerColor = null;
+  let isGameActive = true;
 
-    const WHITE = 'w'
-    const BLACK = 'b'
-    const TIE = 'TIE';
+  const WHITE = "w";
+  const BLACK = "b";
+  const DRAW = "DRAW";
 
-    function format(obj) {
-        return JSON.stringify(obj)
+  function format(obj) {
+    return JSON.stringify(obj);
+  }
+
+  //connect the user to the server
+  const ws = new WebSocket("ws://localhost:8080");
+  ws.onopen = () => {
+    ws.send(format({ type: "init-game", data: {} }));
+  };
+
+  ws.onmessage = (event) => {
+    const { type, data } = JSON.parse(event.data);
+    console.log(
+      `Received event with type ${type} and data ${JSON.stringify(data)}`
+    );
+
+    if (type === "welcome") {
+      // if we ever add more info to the player object sent on connect, change to just data to get all necessary info
+      currentPlayer = data.player;
+      if (currentPlayer === "p2") {
+        // if we are black, flip the ids on the tiles of the board
+        flipBoard();
+        playerColor = BLACK;
+      } else {
+        playerColor = WHITE;
+      }
     }
 
-    //connect the user to the server
-    const ws = new WebSocket("ws://localhost:8080")
-    ws.onopen = () => {
-        ws.send(format({ type: 'init-game', data: {} }))
+    if (type === "init-game") {
+      board = data;
+      syncBoard();
     }
 
-    ws.onmessage = (event) => {
-        const { type, data } = JSON.parse(event.data)
-        console.log(`Received event with type ${type} and data ${JSON.stringify(data)}`)
-
-        if(type === 'welcome') {
-             // if we ever add more info to the player object sent on connect, change to just data to get all necessary info
-             currentPlayer = data.player;
-             if(currentPlayer === 'p2'){ // if we are black, flip the ids on the tiles of the board
-                flipBoard();
-             }
-        }
-
-        if (type === 'init-game') {
-            board = data
-            syncBoard()
-        }
-
-        if (type === 'move-made') {
-            board = data.board
-            syncBoard()
-        }
-
-        // TODO: Implement on server
-        if (type === 'game-over') {
-            const winner = data.winner
-            board = data.board
-            syncBoard()
-            isGameActive = false
-            announce(winner)
-        }
-
-        if (type === 'game-reset') {
-            board = data.board
-            syncBoard()
-            hideAnnounce()
-            isGameActive = true;
-        }
+    if (type === "move-made") {
+      board = data.board;
+      syncBoard();
     }
 
-    function colorBoard() {
-        for (let i = 0; i < tiles.length; i++) {
-            if (i % 2 === 0) {
-                if (Math.floor(i / 8) % 2 === 0) {
-                    tiles[i].style.backgroundColor = 'rgba(166, 209, 248, 0.75)'
-                } else {
-                    tiles[i].style.backgroundColor = 'rgba(104, 180, 252, 0.75)'
-                }
+    // TODO: Implement on server
+    if (type === "game-over") {
+      const winner = data.winner;
+      board = data.board;
+      syncBoard();
+      isGameActive = false;
+      announce(winner);
+    }
 
-            } else {
-                if (Math.floor(i / 8) % 2 === 0) {
-                    tiles[i].style.backgroundColor = 'rgba(104, 180, 252, 0.75)'
-                } else {
-                    tiles[i].style.backgroundColor = 'rgba(166, 209, 248, 0.75)'
-                }
-            }
+    if (type === "game-reset") {
+      board = data.board;
+      syncBoard();
+      hideAnnounce();
+      isGameActive = true;
+    }
+  };
+
+  function colorBoard() {
+    for (let i = 0; i < tiles.length; i++) {
+      if (i % 2 === 0) {
+        if (Math.floor(i / 8) % 2 === 0) {
+          tiles[i].style.backgroundColor = "rgba(166, 209, 248, 0.75)";
+        } else {
+          tiles[i].style.backgroundColor = "rgba(104, 180, 252, 0.75)";
+        }
+      } else {
+        if (Math.floor(i / 8) % 2 === 0) {
+          tiles[i].style.backgroundColor = "rgba(104, 180, 252, 0.75)";
+        } else {
+          tiles[i].style.backgroundColor = "rgba(166, 209, 248, 0.75)";
+        }
+      }
+    }
+  }
+
+  function syncBoard() {
+    for (let i = 0; i < tiles.length; i++) {
+      let [row, col] = toBoardIndex(i);
+      if (board[row][col] !== "") {
+        tiles[
+          i
+        ].innerHTML = `<img class='img-fluid' src='images/${board[row][col]}.png'>`;
+      } else {
+        tiles[i].innerHTML = "";
+      }
+    }
+  }
+
+  function toBoardIndex(index) {
+    let row = parseInt(index / 8);
+    let col = parseInt(index % 8);
+    return [row, col];
+  }
+
+  function flipBoard() {
+    for (let i = 0; i < tiles.length / 2; i++) {
+      let temp = tiles[i].id;
+      tiles[i].id = tiles[tiles.length - 1 - i].id;
+      tiles[tiles.length - 1 - i].id = temp;
+    }
+  }
+
+  function tileToPiece(tile) {
+    let row, col
+    for (let i = 0; i < tiles.length; i++) {
+        if(tiles[i] === tile){
+            [row, col] = toBoardIndex(i)
         }
     }
+    return board[row][col]
+  }
 
-    function syncBoard() {
-        for (let i = 0; i < tiles.length; i++) {
-            let [row, col] = toBoardIndex(i)
-            if (board[row][col] !== '') {
-                tiles[i].innerHTML = `<img class='img-fluid' src='images/${board[row][col]}.png'>`;
-            } else {
-                tiles[i].innerHTML = '';
-            }
-        }
+  const announce = (type) => {
+    switch (type) {
+      case WHITE:
+        announcer.innerHTML = "White Wins!";
+        break;
+      case BLACK:
+        announcer.innerHTML = "Black Wins!";
+        break;
+      case DRAW:
+        announcer.innerText = "Draw...";
     }
+    announcer.classList.remove("hide");
+  };
 
-    function toBoardIndex(index) {
-        let row = parseInt(index / 8)
-        let col = parseInt(index % 8)
-        return [row, col]
+  const hideAnnounce = () => {
+    announcer.classList.add("hide");
+  };
+
+  const isValidAction = (tile) => {
+    // TODO: Some basic validation later. But let server do the validation for now
+    return true;
+  };
+
+  let lastClickedTile = null;
+  const userAction = (tile, index) => {
+    console.log(tile);
+    if (isValidAction(tile) && isGameActive) {
+      if (!lastClickedTile || tileToPiece(lastClickedTile) === '' || tileToPiece(tile)[0] === playerColor) {   //if no previous tile, if from is empty, if to is of same color
+        colorBoard()
+        tile.style.backgroundColor = "rgba(0, 29, 54, .75)";
+        lastClickedTile = tile;
+      } else if (lastClickedTile === tile) {
+        colorBoard();
+        lastClickedTile = null;
+      }  else {
+        const from = lastClickedTile.id;
+        const to = tile.id;
+        colorBoard();
+        lastClickedTile = null;
+        ws.send(format({ type: "make-move", data: { to, from } }));
+      }
     }
+  };
 
-    function flipBoard(){
-        for(let i = 0; i < tiles.length/2; i++){
-            let temp = tiles[i].id
-            tiles[i].id = tiles[tiles.length - 1 - i].id
-            tiles[tiles.length - 1 - i].id = temp
-        }
-    }
+  tiles.forEach((tile, index) => {
+    tile.addEventListener("click", () => userAction(tile, index));
+    // tile.addEventListener('click', audio.play);
+  });
 
-    const announce = (type) => {
-        switch (type) {
-            case WHITE:
-                announcer.innerHTML = 'White Wins!';
-                break;
-            case BLACK:
-                announcer.innerHTML = 'Black Wins!';
-                break;
-            case TIE:
-                announcer.innerText = 'Tie...';
-        }
-        announcer.classList.remove('hide');
-    };
+  function resetGame() {
+    ws.send(format({ type: "reset-game", data: {} }));
+  }
 
-    const hideAnnounce = () => {
-        announcer.classList.add('hide');
-    }
-
-    const isValidAction = (tile) => {
-        // TODO: Some basic validation later. But let server do the validation for now
-        return true;
-    };
-
-    let lastClickedTile = null
-    const userAction = (tile, index) => {
-        console.log(tile)
-        if (isValidAction(tile) && isGameActive) {
-            if (!lastClickedTile) {
-                tile.style.backgroundColor = "rgba(0, 29, 54, .75)";
-                lastClickedTile = tile;
-            } else if (lastClickedTile === tile) {
-                colorBoard()
-                lastClickedTile = null;
-            } else {
-                const from = lastClickedTile.id
-                const to = tile.id
-                colorBoard()
-                lastClickedTile = null;
-                ws.send(format({ type: 'make-move', data: { to, from } }))
-            }
-        }
-    }
-
-    tiles.forEach((tile, index) => {
-        tile.addEventListener('click', () => userAction(tile, index));
-        // tile.addEventListener('click', audio.play);
-    });
-
-    function resetGame() {
-        ws.send(format({ type: 'reset-game', data: {} }))
-    }
-
-    resetButton.addEventListener('click', resetGame);
+  resetButton.addEventListener("click", resetGame);
 });
--- server/index.js
+++ server/index.js
@@ -1,184 +1,225 @@
 // TODO: user emit instead of sending to each connection (might need socket.io)
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from "ws";
 
 let startingBoard = [
-    ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR',],
-    ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP',],
-    ['', '', '', '', '', '', '', '',],
-    ['', '', '', '', '', '', '', '',],
-    ['', '', '', '', '', '', '', '',],
-    ['', '', '', '', '', '', '', '',],
-    ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP',],
-    ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR',],
-]
+  ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+  ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+  ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+];
 
 const game = {
-    board: null,
-    p1: null,
-    p2: null,
-    spec: [],
-    turn: 'w'
-}
+  board: null,
+  p1: null,
+  p2: null,
+  spec: [],
+  turn: "w",
+};
 
 function getFileRank(mv) {
-    return [mv[0], parseInt(mv[1])]
+  return [mv[0], parseInt(mv[1])];
 }
 
 function getIndexesGivenFileRank({ file, rank }) {
-    const col = file.charCodeAt(0) - 'a'.charCodeAt(0)
-    const row = 8 - rank
-    return [row, col]
+  const col = file.charCodeAt(0) - "a".charCodeAt(0);
+  const row = 8 - rank;
+  return [row, col];
 }
 
-function reverseBoard(board){
-    let result = board.toReversed()
-    for(let i = 0; i < board.length; i++){
-        result[i] = result[i].toReversed();
-    }
-    return result;
+function reverseBoard(board) {
+  let result = board.toReversed();
+  for (let i = 0; i < board.length; i++) {
+    result[i] = result[i].toReversed();
+  }
+  return result;
 }
 
 function makeMove({ to, from }) {
-    console.log(`Making move from ${from} to ${to}...`)
-    const [fromFile, fromRank] = [from[0], parseInt(from[1])]
-    const [toFile, toRank] = [to[0], parseInt(to[1])]
+  console.log(`Making move from ${from} to ${to}...`);
+  const [fromFile, fromRank] = [from[0], parseInt(from[1])];
+  const [toFile, toRank] = [to[0], parseInt(to[1])];
 
-    const [fromRow, fromCol] = getIndexesGivenFileRank({ file: fromFile, rank: fromRank })
-    console.log('fromRow', fromRow, 'fromCol', fromCol)
-    const [toRow, toCol] = getIndexesGivenFileRank({ file: toFile, rank: toRank })
-    console.log('toRow', toRow, 'toCol', toCol)
+  const [fromRow, fromCol] = getIndexesGivenFileRank({
+    file: fromFile,
+    rank: fromRank,
+  });
+  console.log("fromRow", fromRow, "fromCol", fromCol);
+  const [toRow, toCol] = getIndexesGivenFileRank({
+    file: toFile,
+    rank: toRank,
+  });
+  console.log("toRow", toRow, "toCol", toCol);
 
-    game.board[toRow][toCol] = game.board[fromRow][fromCol]
-    game.board[fromRow][fromCol] = ''
+  game.board[toRow][toCol] = game.board[fromRow][fromCol];
+  game.board[fromRow][fromCol] = "";
 }
 
 function toggleTurn() {
-    game.turn = game.turn === 'w' ? 'b' : 'w'
+  game.turn = game.turn === "w" ? "b" : "w";
 }
 
 function isTurn(ws) {
-    if (game.turn === 'w') return ws === game.p1
-    return ws === game.p2
+  if (game.turn === "w") return ws === game.p1;
+  return ws === game.p2;
 }
 
 function isValidMove({ to, from }) {
-    console.log(`Checking if valid from ${from} to ${to}...`)
-    const [fromFile, fromRank] = [from[0], parseInt(from[1])]
-    const [toFile, toRank] = [to[0], parseInt(to[1])]
+  console.log(`Checking if valid from ${from} to ${to}...`);
+  const [fromFile, fromRank] = [from[0], parseInt(from[1])];
+  const [toFile, toRank] = [to[0], parseInt(to[1])];
 
-    const [fromRow, fromCol] = getIndexesGivenFileRank({ file: fromFile, rank: fromRank })
-    const [toRow, toCol] = getIndexesGivenFileRank({ file: toFile, rank: toRank })
+  const [fromRow, fromCol] = getIndexesGivenFileRank({
+    file: fromFile,
+    rank: fromRank,
+  });
+  const [toRow, toCol] = getIndexesGivenFileRank({
+    file: toFile,
+    rank: toRank,
+  });
 
-    // check rows
-    if (fromRow < 0 || toRow < 0 || fromRow >= 8 || toRow >= 8) return false
+  // check rows
+  if (fromRow < 0 || toRow < 0 || fromRow >= 8 || toRow >= 8) return false;
 
-    // check cols
-    if (fromCol < 0 || toCol < 0 || fromCol >= 8 || toCol >= 8) return false
+  // check cols
+  if (fromCol < 0 || toCol < 0 || fromCol >= 8 || toCol >= 8) return false;
 
-    // CHECK IF FROM IS EMPTY
+  // CHECK IF FROM IS EMPTY
 
-    // More chess logic...
+  // More chess logic...
 
-    return true
+  return true;
 }
 
-
 function handleMessage(ws, payload) {
-    console.log(`Recieved: ${payload}`);
-    const { type, data } = JSON.parse(payload)
+  console.log(`Recieved: ${payload}`);
+  const { type, data } = JSON.parse(payload);
+    
+  switch (type) {
+    case "init-game":
+        // BUG: if white plays a move then black connects, black does not see the move then the board resets as if black has first move
+      game.board = structuredClone(startingBoard);
+      if (ws === game.p2) {
+        ws.send(
+          format({
+            type: "init-game",
+            data: structuredClone(reverseBoard(game.board)),
+          })
+        );
+      } else {
+        ws.send(
+          format({ type: "init-game", data: structuredClone(game.board) })
+        );
+      }
+      return;
+    case "make-move":
+      if (!isTurn(ws)) {
+        ws.send(
+          format({
+            type: "invalid-move",
+            data: {
+              message: "Not your move bruv",
+            },
+          })
+        );
+        return;
+      }
+      if (!isValidMove(data)) {
+        ws.send(
+          format({
+            type: "invalid-move",
+            data: {
+              message: `Move from ${data.from} to ${data.to} is not valid`,
+            },
+          })
+        );
+        return;
+      }
 
-    switch (type) {
-        case 'init-game':
-            game.board = structuredClone(startingBoard)
-            if (ws === game.p2) {
-                // TODO: Reverse the board before sending
-                ws.send(format({ type: 'init-game', data: structuredClone(reverseBoard(game.board)) }));
-            } else {
-                ws.send(format({ type: 'init-game', data: structuredClone(game.board) }));
-            }
-            return
-        case 'make-move':
-            if (!isTurn(ws)) {
-                ws.send(format({
-                    type: 'invalid-move', data: {
-                        message: "Not your move bruv"
-                    }
-                }));
-                return
-            }
-            if (!isValidMove(data)) {
-                ws.send(format({
-                    type: 'invalid-move', data: {
-                        message: `Move from ${data.from} to ${data.to} is not valid`
-                    }
-                }));
-                return
-            }
+      makeMove(data);
+      toggleTurn(game);
+      if (game.p1) {
+        game.p1.send(
+          format({ type: "move-made", data: { board: game.board } })
+        ); //send to white
+      }
+      if (game.p2) {
+        game.p2.send(
+          format({
+            type: "move-made",
+            data: { board: reverseBoard(game.board) },
+          })
+        ); //send reversed board to black
+      }
 
-            makeMove(data)
-            toggleTurn(game)
-            if (game.p1) {
-                game.p1.send(format({ type: 'move-made', data: { board: game.board } }));   //send to white
-            }
-            if (game.p2) {
-                // TODO: Reverse board later
-                game.p2.send(format({ type: 'move-made', data: { board: reverseBoard(game.board) } }));   //send reversed board to black
-            }
+      for (let client of game.spec) {
+        client.send(format({ type: "move-made", data: { board: game.board } })); //send to each spec
+      }
+      return;
+    case "reset-game":
+      game.board = structuredClone(startingBoard);
+      game.turn = "w"
+      if (game.p1) {
+        game.p1.send(
+          format({ type: "game-reset", data: { board: game.board } })
+        ); //send to white
+      }
+      if (game.p2) {
+        game.p2.send(
+          format({
+            type: "game-reset",
+            data: { board: reverseBoard(game.board) },
+          })
+        ); //send reversed board to black
+      }
 
-            for (let client of game.spec) {
-                client.send(format({ type: 'move-made', data: { board: game.board } }));    //send to each spec
-            }
-            return
-        case 'reset-game':
-            game.board = structuredClone(startingBoard)
-            if (game.p1) {
-                game.p1.send(format({ type: 'game-reset', data: { board: game.board } }));   //send to white
-            }
-            if (game.p2) {
-                // TODO: Reverse board later
-                game.p2.send(format({ type: 'game-reset', data: { board: reverseBoard(game.board) } }));   //send reversed board to black
-            }
-
-            for (let client of game.spec) {
-                client.send(format({ type: 'game-reset', data: { board: game.board } }));    //send to each spec
-            }
-            return
-        default:
-            ws.send(format({ type: 'unkown-message', data: `Uknown message with type ${type} and data ${data}` }));
-    }
+      for (let client of game.spec) {
+        client.send(
+          format({ type: "game-reset", data: { board: game.board } })
+        ); //send to each spec
+      }
+      return;
+    default:
+      ws.send(
+        format({
+          type: "unkown-message",
+          data: `Uknown message with type ${type} and data ${data}`,
+        })
+      );
+  }
 }
 
 function format(data) {
-    return JSON.stringify(data)
+  return JSON.stringify(data);
 }
 
 function handleConnect(ws) {
-    if (!game.p1) {
-        //white
-        game.p1 = ws
-        const payload = format({ type: 'welcome', data: { player: 'p1' } })
-        ws.send(payload);
-    } else if (!game.p2) {
-        //black
-        game.p2 = ws
-        const payload = format({ type: 'welcome', data: { player: 'p2' } })
-        ws.send(payload);
-    } else {
-        //spectator
-        game.spec.push(ws);
-        const payload = format({ type: 'welcome', data: { player: 'spec' } })
-        ws.send(payload);
-    }
-
-
+  if (!game.p1) {
+    //white
+    game.p1 = ws;
+    const payload = format({ type: "welcome", data: { player: "p1" } });
+    ws.send(payload);
+  } else if (!game.p2) {
+    //black
+    game.p2 = ws;
+    const payload = format({ type: "welcome", data: { player: "p2" } });
+    ws.send(payload);
+  } else {
+    //spectator
+    game.spec.push(ws);
+    const payload = format({ type: "welcome", data: { player: "spec" } });
+    ws.send(payload);
+  }
 }
 
 const wss = new WebSocketServer({ port: 8080 });
-wss.on('connection', function connection(ws) {
-    handleConnect(ws)
-    ws.on('message', function message(data) {
-        handleMessage(ws, data)
-    });
-
+wss.on("connection", function connection(ws) {
+  handleConnect(ws);
+  ws.on("message", function message(data) {
+    handleMessage(ws, data);
+  });
 });
-console.log('listening')
+console.log("listening");
